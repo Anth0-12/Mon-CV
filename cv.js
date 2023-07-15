de7945cb522cc2dbb99d7f4718188ffa78338c63
Anth0-12 <antho62680@live.fr>
2023-07-15 23:09:31 +0200

    compétences

--- cv.js
+++ cv.js
@@ -1,33 +1,27 @@
 // Obtenez les éléments de la barre de progression
 const progressBars = document.querySelectorAll('.progress-bar');
 
-// Fonction pour mettre à jour la barre de progression
-function updateProgressBar(bar, percentage) {
+// Fonction pour mettre à jour la barre de progression avec une animation
+function updateProgressBarWithAnimation(bar, percentage) {
   const progress = bar.querySelector('.progress');
   progress.style.width = percentage + '%';
 }
 
-// Exemple de mise à jour des barres de progression avec une animation
-setTimeout(() => {
-  updateProgressBar(progressBars[0], 50); // 50% de progression pour la première barre
-}, 500); // Démarre l'animation après 500 millisecondes (0,5 seconde)
+// Créez une instance de l'Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const progressBar = entry.target;
+      const percentage = progressBar.dataset.progress;
+      updateProgressBarWithAnimation(progressBar, percentage);
 
-setTimeout(() => {
-  updateProgressBar(progressBars[1], 75); // 75% de progression pour la deuxième barre
-}, 1000); // Démarre l'animation après 1000 millisecondes (1 seconde)
+      // Arrêtez d'observer cet élément une fois l'animation déclenchée
+      observer.unobserve(progressBar);
+    }
+  });
+});
 
-setTimeout(() => {
-  updateProgressBar(progressBars[2], 30); // 30% de progression pour la troisième barre
-}, 1500); // Démarre l'animation après 1500 millisecondes (1,5 seconde)
-
-setTimeout(() => {
-  updateProgressBar(progressBars[3], 10); // 30% de progression pour la troisième barre
-}, 500); // Démarre l'animation après 1500 millisecondes (1,5 seconde)
-
-setTimeout(() => {
-  updateProgressBar(progressBars[4], 60); // 30% de progression pour la troisième barre
-}, 1000); // Démarre l'animation après 1500 millisecondes (1,5 seconde)
-
-setTimeout(() => {
-  updateProgressBar(progressBars[5], 20); // 30% de progression pour la troisième barre
-}, 1500); // Démarre l'animation après 1500 millisecondes (1,5 seconde)
+// Parcourez chaque barre de progression et observez-la
+progressBars.forEach((progressBar) => {
+  observer.observe(progressBar);
+});

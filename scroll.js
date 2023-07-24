89d58b465e31a79cdf9405c0096ff4e0225f8b0f
Anth0-12 <antho62680@live.fr>
2023-07-24 22:19:31 +0200

    responsive tel

--- scroll.js
+++ scroll.js
@@ -1,9 +1,10 @@
 // On cible tous les éléments ayant l'id "boutonScrollTop"
 let bouton = document.getElementById("scrollMenu");
+let menuTel = document.getElementById("menuTel");
 
 // On ajoute une fonction onscroll à la fenêtre pour surveiller les événements de défilement 
 // de la page et on appelle la fonction "scrollFunction"
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction(), apparationFunction()};
 
 // Quand l'utilisateur scroll de 20px vers le bas le bouton s'affiche 
 // avec le "display: block" sinon il reste invisible avec le "display: none"
@@ -14,3 +15,11 @@ function scrollFunction() {
     bouton.style.display = "none";
   }
 }
+
+function apparationFunction() {
+  if (document.body.scrollTop > 85 || document.documentElement.scrollTop > 85) {
+    menuTel.style.display = "block";
+  } else {
+    menuTel.style.display = "none";
+  }
+};

78d50112bd1398463ba6a0046acf4cdd313e8627
Anth0-12 <antho62680@live.fr>
2023-12-26 15:57:15 +0100

    Ajout fonction animation scroll sur le clic

--- scroll.js
+++ scroll.js
@@ -23,3 +23,16 @@ function apparationFunction() {
     menuTel.style.display = "none";
   }
 };
+
+
+//Animation clic scroll
+let $root = $('html, body');
+      $('a[href^="#"]').click(function() {
+          let href = $.attr(this, 'href');
+          $root.animate({
+            scrollTop: $(href).offset().top
+          }, 500, function() {
+            window.location.hash = href;
+          });
+          return false;
+        });
